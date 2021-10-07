5fc53324c820e8821ceebb9a6c8aa10443e365b2
AYushgupta4387 <ayushgupta4387@gmail.com>
2021-10-07 11:41:27 +0530

    add some comments in the app.js file

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
+// I tried using the .toggle() function instead of this big code, but .toggle() was not working, and I was unable to understand the reason behind.
+
+// For desktop view
 const toggleBtn = document.querySelector(".header__bars-btn");
 const sidebar = document.querySelector(".side-bar");
 
+// For mobile view
 const toggleBtn_2 = document.querySelector(".sb-bars");
 
 toggleBtn.addEventListener("click", function () {
